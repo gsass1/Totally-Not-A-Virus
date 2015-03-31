58ff49115e14acb7fc449b5e4fdce60508ecd107
EinBaum <mail@einbaum.org>
2015-03-31 22:53:58 +0200

    Rework screenshot file management code to use WINAPI with good error checking

--- Client/src/Command_screenshot.cpp
+++ Client/src/Command_screenshot.cpp
@@ -16,42 +16,64 @@ Command_screenshot::~Command_screenshot()
 
 bool Command_screenshot::OnExecute(const std::vector<std::tstring> &args)
 {
-	bool ret = true;
+	bool ret = false;
 
-	imageUtils.TakeScreenshot(V_FAKE_TMP1);
-	
-	FILE *fp;
-	errno_t error = _tfopen_s(&fp, V_FAKE_TMP1, _T("rb"));
-	if(error != 0) {
-		VLog(LERROR, "_tfopen failed");
-		ret = false;
+	bool imageRet = imageUtils.TakeScreenshot(V_FAKE_TMP1);
+	if (!imageRet)
+	{
+		VLog(LERROR, "Failed to take screenshot");
+		return false;
+	}
+
+	HANDLE hFile = CreateFile(V_FAKE_TMP1, GENERIC_READ, NULL, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		VLog(LERROR, "Failed to open screenshot file");
 		goto out_delete_file;
 	}
 
-	fseek(fp, 0, SEEK_END);
-	size_t size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-
-	char* buffer = (char*)malloc(size);
-	if(!buffer)
+	LARGE_INTEGER lsize;
+	if (!GetFileSizeEx(hFile, &lsize))
 	{
-		VLog(LERROR, "Couldn't allocate buffer");
-		ret = false;
-		goto out_close_fp;
+		VLog(LERROR, "Failed to get screenshot file size");
+		goto out_close_file;
 	}
 
-	if(fread_s(buffer, size, 1, size, fp) != size)
+	DWORD size = lsize.LowPart;
+
+	char *buffer = (char*)malloc(size);
+	if (!buffer)
 	{
-		VLog(LERROR, "fread_s did not return size");
-		ret = false;
+		VLog(LERROR, "Failed to allocate screenshot memory");
+		goto out_close_file;
+	}
+
+	DWORD bytesRead;
+	BOOL readRet = ReadFile(hFile, buffer, size, &bytesRead, NULL);
+	if (!readRet)
+	{
+		VLog(LERROR, "Failed to read screenshot file");
 		goto out_free_buffer;
 	}
-	//HANDLE hFile = CreateFile(V_FAKE_TMP1, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
-	network.SendFile(V_NET_FILE_DATA, size, buffer);
+	if (bytesRead != size)
+	{
+		VLog(LERROR, "Failed to read screenshot file; bytesRead != size");
+		goto out_free_buffer;
+	}
+
+	bool networkRet = network.SendFile(V_NET_FILE_DATA, size, buffer);
+	if (!networkRet)
+	{
+		VLog(LERROR, "Failed to send screenshot file; network error");
+		goto out_free_buffer;
+	}
+
+	ret = true;
 
 out_free_buffer: free(buffer);
-out_close_fp:    fclose(fp);
+out_close_file:  CloseHandle(hFile);
 out_delete_file: DeleteFile(V_FAKE_TMP1);
 	return ret;
 }
