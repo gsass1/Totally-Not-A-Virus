a5fcfccd60b6a10622f16f66b5ac57e68cc824ed
Gian Sass <gian.sass@outlook.de>
2015-07-08 20:17:39 +0200

    Improve screenshot code

--- Client/src/ImageUtils.h
+++ Client/src/ImageUtils.h
@@ -1,17 +1,17 @@
 #pragma once
 #include "stdafx.h"
 
-
 class ImageUtils
 {
 public:
 	ImageUtils();
 	~ImageUtils();
 
-	bool TakeScreenshot(const std::wstring& filepath);
+	bool TakeScreenshot(unsigned char **buffer, DWORD *size);
 
 private:
 	int GetEncoderClsid(const wchar_t* format, CLSID* pClsid);
+	bool SaveGdiImageToMemory(unsigned char **memDst, DWORD *size, Gdiplus::Bitmap *bmp);
 
 	Gdiplus::GdiplusStartupInput gdiplusStartupInput;
 	unsigned long gdiPlusToken;
